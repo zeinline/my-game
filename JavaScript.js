7af750296992a23f51d01742d695515ef0ca398f
zeinline <71123844+zeinline@users.noreply.github.com>
2021-02-16 07:30:19 +0600

    финальный апгрейд 16 02 2021

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,27 @@
-function buttonClick(){
-   
+var Global_Menu = {
+  name: "name",
+  version: "v 0",
+  size: "0 mb"
+};
+
+var All_Game = {
+  name: "All Game",
+  version: "v 1.0",
+  size: "13.1 mb"
+};
+
+var Zodiacs = {
+  name: "Zodiacs",
+  version: "v 1.0",
+  size: "12.3 mb"
+};
+
+window.onload = function() {
+  //bt_win_l(0);
+}
+
+function bt_win_l (indexGame) {
+  document.getElementById("nameGame").innerHTML = (All_Game.name);
+  document.getElementById("versionGame").innerHTML = (All_Game.version);
+  document.getElementById("sizeGame").innerHTML = (All_Game.size);
 }
